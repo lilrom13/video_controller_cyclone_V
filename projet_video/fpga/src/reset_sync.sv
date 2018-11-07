b1089d1151ae950b0dee5e84a58298368c77d81c
Romain Margheriti <romain.margheriti@telecom-paristech.fr>
2018-11-07 12:56:48 +0100

    Q 9) Reset avec deux bascules d simu -> OK, synt -> OK

---
+++ projet_video/fpga/src/reset_sync.sv
@@ -0,0 +1,17 @@
+`default_nettype none
+module reset_sync(input wire clk,
+		  input wire nrst,
+		  output logic n_rst);
+   logic [1:0] bd;
+
+   assign n_rst = bd[1];
+   
+   always @ (posedge clk or negedge nrst)
+     if (!nrst) bd <= 0;
+     else
+       begin
+	  bd[0] <= 1;
+	  bd[1] <= bd[0];
+	  // bd <= {bd[0],1'b1};
+       end
+endmodule
